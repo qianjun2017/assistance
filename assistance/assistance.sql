d01c30925699abcc6a97b6c5fc51cbaba1e487f1
qianjun2017 <ws_yuan@126.com>
2019-02-17 19:24:52 +0800

    修改

--- assistance/assistance.sql
+++ assistance/assistance.sql
@@ -383,6 +383,31 @@ LOCK TABLES `t_lottery_customer` WRITE;
 /*!40000 ALTER TABLE `t_lottery_customer` DISABLE KEYS */;
 /*!40000 ALTER TABLE `t_lottery_customer` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `t_system_config`
+--
+
+DROP TABLE IF EXISTS `t_system_config`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_system_config` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `propertyName` varchar(64) DEFAULT NULL,
+  `propertyValue` varchar(512) DEFAULT NULL,
+  `propertyDesc` varchar(512) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `t_system_config`
+--
+
+LOCK TABLES `t_system_config` WRITE;
+/*!40000 ALTER TABLE `t_system_config` DISABLE KEYS */;
+/*!40000 ALTER TABLE `t_system_config` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
