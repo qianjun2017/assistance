41027fba216f36bc728d0e4e2352197acc79a443
qianjun2017 <ws_yuan@126.com>
2019-02-17 19:06:46 +0800

    客户开启商家服务

--- assistance/src/main/java/com/cc/customer/web/CustomerController.java
+++ assistance/src/main/java/com/cc/customer/web/CustomerController.java
@@ -4,18 +4,21 @@
 package com.cc.customer.web;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.cc.common.exception.LogicException;
 import com.cc.common.tools.ListTools;
+import com.cc.common.tools.StringTools;
 import com.cc.common.web.Page;
 import com.cc.common.web.Response;
 import com.cc.customer.service.CustomerService;
@@ -140,6 +143,51 @@ public class CustomerController {
 	public Page<CustomerBean> queryCustomerPage(@ModelAttribute CustomerQueryForm form){
 		Page<CustomerBean> page = customerService.queryCustomerPage(form);
 		return page;
+	}
+	
+	/**
+	 * 客户开启商家服务
+	 * @param retailerMap
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping(value = "/retailer", method = RequestMethod.POST)
+	public Response<Object> retailer(@RequestBody Map<String, String> retailerMap){
+		Response<Object> response = new Response<Object>();
+		CustomerBean customerBean = CustomerBean.get(CustomerBean.class, Long.valueOf(retailerMap.get("customerId")));
+		if (customerBean == null) {
+			response.setMessage("您尚未注册");
+			return response;
+		}
+		String store = retailerMap.get("store");
+		if(StringTools.isNullOrNone(store)){
+			response.setMessage("请输入店铺名称");
+			return response;
+		}
+		customerBean.setStore(store);
+		String phone = retailerMap.get("phone");
+		if(StringTools.isNullOrNone(phone)){
+			response.setMessage("请输入联系电话");
+			return response;
+		}
+		customerBean.setPhone(phone);
+		String address = retailerMap.get("address");
+		if(StringTools.isNullOrNone(address)){
+			response.setMessage("请输入店铺地址");
+			return response;
+		}
+		customerBean.setAddress(address);
+		customerBean.setRetailer(Boolean.TRUE);
+		try {
+			customerService.saveCustomer(customerBean);
+			response.setSuccess(Boolean.TRUE);
+		} catch (LogicException e) {
+			response.setMessage(e.getErrContent());
+		} catch (Exception e) {
+			response.setMessage("系统内部错误");
+			e.printStackTrace();
+		}
+		return response;
 	}
 
 }
